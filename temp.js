2676e079450c0857c2c37b0af04eab0b2400a80a
utmalik <utmalik@amazon.com>
2023-04-23 17:11:36 +0530

    execution context

--- temp.js
+++ temp.js
@@ -1,64 +1,23 @@
+let numberOfBagels ;
 
-
-// function add(a,b){
-//     return a+b;
-// }
-
-// //functions are first class citizens in javascript 
-
-
-// function average(a,b,f){
-//     return f(a,b)/2;
-// }
-
-// const result = average(4,7,add);
-
-// console.log(result);
-
-// function a(){
-
-//     function b(){
-
-//        return 10;
-//     }
-
-//     return b;
-// }
-
-// const result = a()();
-
-// console.log(result);
-
-
-
-
-const f = function add(a,b){
-    return a+b;
+function eatBagel() {
+  numberOfBagels -= 1;
+  if (numberOfBagels === 0) {
+    console.log('Oh nO, Out Of bagles!');
+  } else {
+    console.log(`Yummy. There are ${numberOfBagels} left!`);
+  }
 }
+function Bagel() {
+    console.log(x);
+    let x=5;
+  }
 
-console.log(f(4,5));
+eatBagel();
+Bagel();
+eatBagel();
+Bagel();
+Bagel();
 
-//we can assign functions to variables
-// we can pass functions as arguements to another functions 
-// we can even return functions from functions 
+console.log(numberOfBagels);
 
-
-
-
-
-
-
-// function a(){
-
-//     return function b(){
-
-
-//         return function c(){
-
-//             console.log("hello");
-
-//         }
-//     }
-// }
-
-// a()()();
