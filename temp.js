28fb3151fcfd5b63b18335402c7a2b6b34360684
utmalik <utmalik@amazon.com>
2023-04-23 18:54:49 +0530

    all about objects

--- temp.js
+++ temp.js
@@ -1,23 +1,208 @@
-let numberOfBagels ;
 
-function eatBagel() {
-  numberOfBagels -= 1;
-  if (numberOfBagels === 0) {
-    console.log('Oh nO, Out Of bagles!');
-  } else {
-    console.log(`Yummy. There are ${numberOfBagels} left!`);
-  }
-}
-function Bagel() {
-    console.log(x);
-    let x=5;
-  }
 
-eatBagel();
-Bagel();
-eatBagel();
-Bagel();
-Bagel();
+//IIFEs 
+// Immidiately invoked function expressions
 
-console.log(numberOfBagels);
+
+
+// (function(){
+//     console.log("hello");
+// })();
+
+
+
+//arrow functions
+
+// const add = function(a,b){
+//     return a+b;
+// }
+
+// const add =  (a,b) => a+b;
+
+// console.log(add(3,4));
+
+
+
+// let person = {
+
+//     firstName:"Utkarsh",
+//     lastName:"Malik", 
+//     age:27,
+//     sayHi: function(){
+//         console.log("Hii");
+//     },
+//     getFullName:function(){
+
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
+
+// console.log(person.getFullName());
+
+
+// const getFullName = person.getFullName;
+
+// const fullName= getFullName();
+// console.log(fullName);
+
+
+
+
+//this keywords : 
+
+// value of this keyword inside of methods point to the object with which
+//that object is called with 
+
+
+
+
+
+
+//this -> window 
+
+
+var a = "hello";
+
+// console.log(this.a);
+
+
+
+
+
+//constructor functions 
+
+// let person = {
+
+//     firstName:"Utkarsh",
+//     lastName:"Malik"
+// // }
+
+
+
+// function Person(firstName,lastName){
+//     this.firstName=firstName;
+//     this.lastName=lastName;
+//     this.getFullName= function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
+
+// let person1 = new Person("Utkarsh", "Malik");
+
+// let person2  = new Person("Rahul", "Jain");
+
+// console.log(person1);
+// console.log(person2);
+
+// console.log(person1.toString());
+
+
+
+
+
+// function Person(firstName,lastName){
+//     this.firstName=firstName;
+//     this.lastName=lastName;
+// }
+
+// let person1 = new Person("Utkarsh", "Malik");
+
+// let person2  = new Person("Rahul", "Jain");
+
+
+
+// Person.prototype.getFullName = ()=>{
+//     console.log("hello");
+// }
+
+// console.log(Person.prototype);
+
+
+
+
+
+// const obj = {name:"Utkarsh"};
+
+// const obj = new Object({name:"Utkarsh"});
+
+// console.log(Object.prototype);
+
+// console.log(obj.toString());
+
+
+
+// function Person(firstName,lastName){
+//     this.firstName=firstName;
+//     this.lastName=lastName;
+// }
+
+// let person1 = new Person("Utkarsh", "Malik");
+
+// let person2  = new Person("Rahul", "Jain");
+
+
+
+// Person.prototype.getFullName = ()=>{
+//     console.log("hello");
+// }
+
+// console.log(person1.toString());
+
+
+// console.log(person1.__proto__.__proto__ === Object.prototype);
+// console.log(Person.prototype.__proto__ === Object.prototype);
+
+
+
+// let arr=["1","12","12342"]
+// let arr = new Array("1","12","12342");
+
+// console.log(typeof arr);
+
+
+// console.log(arr.__proto__ === Array.prototype);
+
+// arr.toString = ()=> 5;
+
+// console.log(arr.toString());
+
+// console.log(Object.prototype.__proto__);
+
+
+
+//methods : forEach, map, filter, reduce 
+
+
+// call apply and bind are methods 
+
+// const person1={
+//     firstName:"Utkarsh",
+//     lastName:"Malik"
+// }
+
+
+// const person2={
+//     firstName:"Rahul",
+//     lastName:"Jain"
+// }
+
+// function getFullName(age,x){
+//     return this.firstName + " " +  this.lastName + age +x;
+// }
+
+
+// const fullName = getFullName.call(person1,10,20);
+// console.log(fullName);
+
+
+// const fullNameWIthApply = getFullName.apply(person1,[10,20]);
+// console.log(fullNameWIthApply);
+
+
+// const getFullNameFn = getFullName.bind(person1);
+
+// getFullNameFn(10,20);
+
+
+
 
